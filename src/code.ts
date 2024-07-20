6c4addf9e3003ee7db7e45db99502d0f9df28fb2
Alex González <alejandrogsa@gmail.com>
2024-07-20 12:59:53 -0300

    create translator plugin

--- src/code.ts
+++ src/code.ts
@@ -9,24 +9,37 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
+type PluginMessage = {
+  lang: string;
+  description: string;
+};
+
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+figma.ui.onmessage = async (msg: PluginMessage) => {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  const textNode: any = figma.currentPage.findOne(
+    (n) => n.name === "DESCRIPTION"
+  );
+  if (textNode) {
+    textNode.characters = msg.description;
+    figma.notify(`Text has changed to ${msg.lang}`);
   }
+  // if (msg.type === 'create-rectangles') {
+  //   const nodes: SceneNode[] = [];
+  //   for (let i = 0; i < msg.count; i++) {
+  //     const rect = figma.createRectangle();
+  //     rect.x = i * 150;
+  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+  //     figma.currentPage.appendChild(rect);
+  //     nodes.push(rect);
+  //   }
+  //   figma.currentPage.selection = nodes;
+  //   figma.viewport.scrollAndZoomIntoView(nodes);
+  // }
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
